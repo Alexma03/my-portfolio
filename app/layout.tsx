ae66c3c678f8ea555e2fd7476ab81c02e2fd59d8
Alex-PC <alex03marcos@gmail.com>
2024-09-20 12:39:03 +0200

    Add dock and theme toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { AuroraBackgroundDemo } from "../components/AuroraBackground";
+import { DockDemo } from "@/components/Dock";
+import ThemeWrapper from "@/components/ThemeWrapper";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +16,10 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body>
-        <AuroraBackgroundDemo />
-        {children}
+        <ThemeWrapper>
+          {children}
+          <DockDemo />
+        </ThemeWrapper>
       </body>
     </html>
   );

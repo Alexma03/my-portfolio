a5acfdaa46197ab4ba49eea6032f9ca83d47b17a
Alex-PC <alex03marcos@gmail.com>
2024-09-21 02:12:12 +0200

    Update grid layout styles in page.tsx and tailwind.config.ts

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,11 @@ export default function Home() {
           duration: 0.8,
           ease: "easeInOut",
         }}
-        className="relative grid gap-6 py-32 px-10 max-w-[1675px] mx-auto h-[100vh] w-full grid-cols-12 grid-rows-12 xl:py-10 lg:py-24 2xl:py-32 2xl:gap-8"
+        className="
+          relative grid gap-6 py-8 px-16 max-w-[1675px] mx-auto w-full h-full sm:px-32
+          grid-cols-1 md:grid-cols-2 lg:px-28 xl:grid-cols-12 xl:grid-rows-12
+          xl:py-10 xl:px-16 lg:py-18 2xl:p-28 2xl:gap-8
+        "
       >
         {cards.map((card, index) => (
           <motion.div
@@ -24,11 +28,7 @@ export default function Home() {
             initial={{ opacity: 0, scale: 0.8 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.5, delay: index * 0.1 }}
-            className={`bg-white bg-opacity-10 rounded-lg shadow-lg backdrop-blur-md col-span-${
-              card.colSpan
-            } row-span-${card.rowSpan} col-start-${card.colStart} row-start-${
-              card.rowStart
-            } ${card.title || card.content ? "p-6" : ""}`}
+            className={ `bg-white bg-opacity-10 rounded-lg shadow-lg backdrop-blur-md ${card.title || card.content ? "p-6" : ""} col-span-1 md:col-span-1 xl:col-span-${card.colSpan} xl:row-span-${card.rowSpan} xl:col-start-${card.colStart} xl:row-start-${card.rowStart}` }
           >
             {card.component ? (
               <card.component /> // Renderiza el componente aquí

07df90e85e074abab8f21a9847409d480a0b2457
Alex-PC <alex03marcos@gmail.com>
2024-09-29 01:22:41 +0200

    Add NameCard, Fix StudiesTimeLine

---
+++ components/NameCard.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { EvervaultCard, Icon } from "./ui/evervault-card";
+
+export function NameCard() {
+  return (
+    <div className="flex flex-rew w-full h-full">
+      <EvervaultCard title="Ingeniero de Software" subTitle="Alejandro Marcos"/>
+    </div>
+  );
+}
--- components/StudiesTimeline.tsx
+++ components/StudiesTimeline.tsx
@@ -1,33 +1,47 @@
 import { Timeline, Badge } from "flowbite-react";
 import { studies } from "@/data/studiesTimelineData";
+import useWindowSize from "@/hooks/useWindowSize"; // Asegúrate de importar el hook correctamente
 
 export default function StudiesTimeline() {
+  const size = useWindowSize();
+  const isHorizontal = (size.width ?? 0) > 768; 
+
   return (
     <div className="h-full w-full rounded-lg p-10 bg-white bg-opacity-10">
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {studies.map((study) => (
-          <div key={study.id} className="col-span-1">
-            <Timeline horizontal>
-                <Timeline.Item>
+          <div key={study.id}>
+            <Timeline horizontal={isHorizontal}>
+              <Timeline.Item className="mb-0 md:ml-6">
                 <Timeline.Point />
                 <Timeline.Content className="flex flex-col justify-normal">
-                  <div className="flex justify-normal my-4">
-                  <img src={study.img} alt={study.title} className="h-8 object-contain" />
+                  <div className="flex flex-col">
+                    <div className="flex justify-normal my-2">
+                      <img
+                        src={study.img}
+                        alt={study.title}
+                        className="h-8 object-contain"
+                      />
+                    </div>
+                    <Timeline.Title className="text-left">
+                      {study.title}
+                    </Timeline.Title>
                   </div>
-                  <Timeline.Time className="dark:text-slate-200 text-left pb-1">{study.year}</Timeline.Time>
-                  <Timeline.Title className="text-left pb-1">{study.title}</Timeline.Title>
+                  <Timeline.Time className="dark:text-slate-200 text-left pt-1">
+                    {study.year}
+                  </Timeline.Time>
                   <Timeline.Body className="text-left pb-1">
-                  <p><strong>Institución:</strong> {study.institution}</p>
-                  <div className="flex flex-wrap gap-2 mt-2">
-                    {study.skills.map((skill, index) => (
-                    <Badge key={index} color="info">
-                      {skill}
-                    </Badge>
-                    ))}
-                  </div>
+                    <p>{study.institution}</p>
+                    <div className="flex flex-wrap gap-2 mt-2">
+                      {study.skills.map((skill, index) => (
+                        <Badge key={index} color="info">
+                          {skill}
+                        </Badge>
+                      ))}
+                    </div>
                   </Timeline.Body>
                 </Timeline.Content>
-                </Timeline.Item>
+              </Timeline.Item>
             </Timeline>
           </div>
         ))}

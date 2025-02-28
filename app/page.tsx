f8037062a54078cbd95f2be99d9a1a75fe42c675
Alex-PC <alex03marcos@gmail.com>
2025-02-28 14:24:13 +0100

    Update layout constraints in Home component for improved responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <section className="sm:h-auto [@media(max-height:1000px)]:xl:h-auto [@media(min-height:1000px)]:xl:h-screen md:h-auto max-h-[1440px]">
+      <section className="sm:h-auto [@media(max-height:1000px)]:xl:h-auto [@media(min-height:1000px)]:xl:h-screen md:h-auto [@media(min-height:1600px)]:h-auto">
         <AuroraBackground>
           <div className="relative z-10 bg-opacity-0 p-4 md:p-6 lg:p-8 xl:p-10 h-full">
             <motion.div

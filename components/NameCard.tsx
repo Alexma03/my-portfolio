843bfb7dd6391a4bc36d8317c0118cfa05f64e97
Alex-PC <alex03marcos@gmail.com>
2025-03-06 15:03:38 +0100

    Fixes in namecard size

--- components/NameCard.tsx
+++ components/NameCard.tsx
@@ -3,9 +3,9 @@ import { EvervaultCard } from "./ui/evervault-card";
 
 export function NameCard() {
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center">
+    <div className="w-full h-[100px] md:h-full flex flex-col justify-center items-center">
       <EvervaultCard 
-        className="my-4 sm:my-5 md:my-6 lg:my-8 xl:my-0"
+        className=""
         text={
           <div className="flex flex-col items-center justify-center">
             <h1 className="text-xl font-bold sm:text-2xl md:text-2xl xl:text-xl 2xl:text-3xl">Hola soy Alejandro Marcos 👋</h1>
